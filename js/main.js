8e4fecf8389377a16ddf4a89e9f1bbf5a6bf2a8c
maxthestranger <thestrangermax@gmail.com>
2022-03-01 19:51:31 +0300

    fix lint errors

--- js/main.js
+++ js/main.js
@@ -1,82 +1,9 @@
+// hambuger menu imports
 const btn = document.querySelector('.mb-menu');
 const menu = document.querySelector('.hamburger');
 const menuList = document.querySelectorAll('.mb-menu-list a');
-const workBtns = document.querySelectorAll('.show-modal');
-const modal = document.querySelector('.modal_container');
-const closeBtn = document.querySelector('.modalcross');
 
-const workData = [
-  {
-    name: 'Multi-Post Stories',
-    description:
-      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    ImgUrl: 'works.png',
-    tech: ['html', 'css', 'js'],
-    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
-    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
-  },
-  {
-    name: 'Project 1',
-    description: 'some value',
-    ImgUrl: 'img/project1.jpg',
-    tech: ['html', 'css', 'js'],
-    demoUrl: 'htps:/.,laksdljsa',
-    gitUrl: 'htosidjosdnks',
-  },
-  {
-    name: 'Project 1',
-    description: 'some value',
-    ImgUrl: 'img/project1.jpg',
-    tech: ['html', 'css', 'js'],
-    demoUrl: 'htps:/.,laksdljsa',
-    gitUrl: 'htosidjosdnks',
-  },
-  {
-    name: 'Project 1',
-    description: 'some value',
-    ImgUrl: 'img/project1.jpg',
-    tech: ['html', 'css', 'js'],
-    demoUrl: 'htps:/.,laksdljsa',
-    gitUrl: 'htosidjosdnks',
-  },
-];
-
-function matchModal(id) {
-  const { ImgUrl, name, description, tech, demoUrl, gitUrl } = workData[id];
-  const modalContainer = document.querySelector('.modal_container');
-  const modal = document.createElement('div');
-  modal.innerHTML = `
-  <div class="modal">
-        <div class="modal_img">
-          <img src="img/${ImgUrl}" alt="${name}" />
-          <img class="modalcross" src="img/Icon-Cancel.svg" alt="crossbtn" />
-        </div>
-
-        <h2 class="modal_title">${name}</h2>
-        <ul class="tags">
-          ${tech.map((tech) => {
-            return `<li>
-            <a href="#"> ${tech} </a>
-          </li>`;
-          })}
-        </ul>
-        <p class="modal_desc">
-          ${description}
-        </p>
-        <div class="modal_btn">
-          <a href="${demoUrl}" class="btn"
-            >See Live <img src="img/export.svg" alt="see live"
-          /></a>
-          <a href="${gitUrl}" class="btn"
-            >See Source <img src="img/github_ic.svg" alt="see live"
-          /></a>
-        </div>
-      </div>
-      `;
-
-  modalContainer.appendChild(modal);
-}
-
+// hambuger menu implementation
 btn.addEventListener('click', () => {
   menu.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
@@ -88,15 +15,131 @@ menuList.forEach((list) => {
   });
 });
 
+// modal implementation
+const modalContainer = document.querySelector('.modal_container');
+const workBtns = document.querySelectorAll('.show-modal');
+
+// modal data
+const workData = [
+  {
+    name: 'Keeping track of hundreds of components 1',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLore",
+    ImgUrl: 'work.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
+    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
+  },
+  {
+    name: 'Keeping track of hundreds of components 2',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    ImgUrl: 'work.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
+    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
+  },
+  {
+    name: 'Keeping track of hundreds of components 3',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    ImgUrl: 'work.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
+    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
+  },
+  {
+    name: 'Keeping track of hundreds of components 4',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    ImgUrl: 'work.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
+    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
+  },
+  {
+    name: 'Keeping track of hundreds of components 5',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    ImgUrl: 'work.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
+    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
+  },
+  {
+    name: 'Keeping track of hundreds of components 6',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    ImgUrl: 'work.png',
+    tech: ['Ruby on rails', 'css', 'javascript'],
+    demoUrl: 'https://maxthestranger.github.io/mts_portfolio/',
+    gitUrl: 'https://github.com/maxthestranger/mts_portfolio',
+  },
+];
+
+// create modal
+function generateModal({
+  ImgUrl, name, description, tech, demoUrl, gitUrl,
+}) {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  modal.innerHTML = `
+    <button type="button" class="modalcross">
+      <span></span>
+      <span></span>
+    </button>
+    <div class="modal_img">
+      <img src="img/${ImgUrl}" alt="${name}" />
+    </div>
+
+    <div class="modal_d">
+      <h2 class="modal_title">${name}</h2>
+      <div class="modal_btn desktop">
+        <a href="${demoUrl}" class="btn"
+          ><span>See Live</span> <img src="img/export.svg" alt="see live"
+        /></a>
+        <a href="${gitUrl}" class="btn"
+          ><span>See Source</span> <img src="img/github_ic.svg" alt="see live"
+        /></a>
+      </div>
+    </div>
+    <ul class="tags">
+      ${tech.map(
+    (tec) => `<li>
+        <a href="#"> ${tec} </a>
+      </li>`,
+  )}
+    </ul>
+    <p class="modal_desc">
+      ${description}
+    </p>
+    <div class="modal_btn mobile">
+      <a href="${demoUrl}" class="btn"
+        ><span>See Live</span> <img src="img/export.svg" alt="see live"
+      /></a>
+      <a href="${gitUrl}" class="btn"
+        ><span>See Source</span> <img src="img/github_ic.svg" alt="see live"
+      /></a>
+    </div>
+  `;
+
+  modalContainer.appendChild(modal);
+}
+
+function closeBtn() {
+  document.querySelector('.modalcross').addEventListener('click', () => {
+    modalContainer.classList.remove('active');
+    document.body.classList.remove('no-scroll');
+  });
+}
+
 workBtns.forEach((workBtn) => {
   workBtn.addEventListener('click', () => {
-    matchModal(workBtn.dataset.id);
-    modal.classList.add('active');
-    // document.body.classList.add('no-scroll');
+    modalContainer.innerHTML = '';
+    generateModal(workData[workBtn.dataset.id]);
+    closeBtn();
+    modalContainer.classList.add('active');
+    document.body.classList.add('no-scroll');
   });
-});
-
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('active');
-  // document.body.classList.remove('no-scroll');
 });
